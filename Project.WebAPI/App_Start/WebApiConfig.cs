75efe0acdfd247a8c4e972275f2b7ffc836e3959
MartinasHub <martina.kitin@gmail.com>
2020-06-04 07:56:08 +0200

    Cors added

--- Project.WebAPI/App_Start/WebApiConfig.cs
+++ Project.WebAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Project.WebAPI
 {
@@ -19,6 +20,8 @@ namespace Project.WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("localhost:3000", "*", "*");
 
         }
     }

c4ac49b09b9d92c41e235fe914505f6b4c0018dc
MartinasHub <martina.kitin@gmail.com>
2020-06-05 10:49:16 +0200

    UnitOfWork and API UnitTests

---
+++ Project.Repository/UnitOfWork.cs
@@ -0,0 +1,113 @@
+﻿using Project.DAL;
+using Project.Model.Common;
+using Project.Repository.Common;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Project.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    { 
+        protected VehicleDbContext DbContext { get; private set; }
+
+        public UnitOfWork(VehicleDbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("DbContext");
+            }
+            DbContext = dbContext;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            int result = 0;
+            using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                result = await DbContext.SaveChangesAsync();
+                scope.Complete();
+            }
+            return result;
+        }
+
+        public virtual Task<int> DeleteAsync<T>(Guid id) where T : class, IBaseDomain
+        {
+            var entity = DbContext.Set<T>().Find(id);
+            if (entity == null)
+            {
+                return Task.FromResult(0);
+            }
+            return DeleteAsync<T>(entity);
+        }
+
+        public virtual Task<int> DeleteAsync<T>(T entity) where T : class, IBaseDomain
+        {
+            try
+            {
+                DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+                if (dbEntityEntry.State != EntityState.Deleted)
+                {
+                    dbEntityEntry.State = EntityState.Deleted;
+                }
+                else
+                {
+                    DbContext.Set<T>().Attach(entity);
+                    DbContext.Set<T>().Remove(entity);
+                }
+                return Task.FromResult(1);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public virtual Task<int> InsertAsync<T>(T entity) where T : class, IBaseDomain
+        {
+            try
+            {
+                DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+                if (dbEntityEntry.State != EntityState.Detached)
+                {
+                    dbEntityEntry.State = EntityState.Added;
+                }
+                else
+                {
+                    DbContext.Set<T>().Add(entity);
+                }
+                return Task.FromResult(1);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public virtual Task<int> UpdateAsync<T>(T entity) where T : class, IBaseDomain
+        {
+            try
+            {
+                DbEntityEntry dbEntityEntry = DbContext.Entry(entity);
+                if (dbEntityEntry.State == EntityState.Detached)
+                {
+                    DbContext.Set<T>().Attach(entity);
+                }
+                dbEntityEntry.State = EntityState.Modified;
+                return Task.FromResult(1);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+    }
+}
